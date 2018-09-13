a92f564a042bc28c736c6300457afc29a199a1fa
Sarah Han <sarah.s.han@gmail.com>
2018-09-12 19:34:45 -0700

    functional components practice done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    name: "Sarah",
+    luckyNumber: LuckyNumber()
+}
+
+function LuckyNumber() {
+    return Math.floor((Math.random() * 1000) + 1);
+}
+
+function Greeting(props) {
+    return (
+        <div className="container">
+            <h1>Hello {props.name}</h1>
+            <h2 className="text-muted">Your lucky number is {props.luckyNumber}</h2>
+        </div>
+    )
+}
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting name={user.name} luckyNumber={user.luckyNumber}/>,
     document.getElementById('root')
 );
